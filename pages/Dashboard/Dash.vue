085c274f6c43dbb1b778a18a249fb6e9c9108542
glowyboy <glowyboy01@gmail.com>
2025-03-13 19:48:18 +0100

    first commit

---
+++ pages/Dashboard/Dash.vue
@@ -0,0 +1,305 @@
+<template>
+  <Navbar/>
+  
+   <div class="bg-bg  flex items-center justify-center flex-col gap-5 ">
+       <div class="flex flex-col items-center justify-center ">
+         <p class="font-roboto font-roboto lg:text-5xl md:text-5xl sm:text-3xl text-3xl font-bold text-white text-center">Technology design tools
+         </p>
+           <p class="font-roboto lg:text-5xl md:text-5xl sm:text-3xl text-3xl font-bold text-white text-center"> for engineers and hobbyists</p>
+       </div>
+         <div class=" font-roboto flex flex-col items-center justify-center ">
+          <p class="font-roboto text-white text-xl text-center">
+               Sit elit feugiat turpis sed integer integer accumsan turpis.
+
+           </p>
+           <p class="font-roboto font-roboto text-xl text-white">Sed suspendisse nec lorem mauris.</p>
+         </div>
+           <button class=" bg-btn cursor-pointer px-7 py-2 rounded-lg text-white font-bold hover:text-btn hover:bg-white hover:border-btn hover:border-2 border-2 border-btn">Request Demo</button>
+         <div class=" w-full flex flex-col items-center justify-center relative">
+           
+           <img src="../../assets/images/Lines.png" class=" absolute top-20 w-full ">
+           <img src="../../assets/images/White.png" class=" absolute -bottom-1 w-full ">
+           <img src="../../assets/images/Desktop.png" class="  mt-4 lg:w-[1100px] md:w-[1100px] w-[600px]">    
+         </div>
+   </div>
+   <div class=" flex lg:gap-20 md:gap-20 xl:gap-20 gap-10  items-center justify-center pt-10">
+       <img src="../../assets/images/Platform1.png" class="  sm:w-28 md:w-28 lg:w-28 w-15" >
+       <img src="../../assets/images/Platform2.png" class="  sm:w-28 md:w-28 lg:w-28 w-15" >
+       <img src="../../assets/images/Platform3.png" class="  sm:w-28 md:w-28 lg:w-28 w-15" > 
+       <img src="../../assets/images/Platform4.png" class="  sm:w-28 md:w-28 lg:w-28 w-15" >
+       <img src="../../assets/images/Platform5.png" class="  sm:w-28 md:w-28 lg:w-28 w-15" >
+    </div>
+   <div class="flex lg:flex-row md:flex-row flex-col-reverse px-10 pt-36 gap-10 ">
+     
+     <div class=" w-full ">
+       <img src="../../assets/images/Efficient.png" class="">
+     </div>
+     <div class="      font-roboto flex items-center justify-center flex-col gap-10  text-btn">
+       <p class="font-roboto md:text-6xl lg:text-6xl text-4xl font-bold text-left">Efficient collaboration</p>
+       <p class="font-roboto leading-loose text-left lg:text-xl md:text-sm text-base">Mauris consequat, cursus pharetra et, habitasse rhoncus quis odio ac. In et dolor eu donec maecenas nulla. Cum sed orci, sit pellentesque quisque feugiat cras ullamcorper.</p>
+     </div>
+   </div>
+   
+   <div class=" grid md:grid-cols-2 lg:grid-cols-2 grid-cols-1 px-10 py-20 gap-10">
+     <div class=" flex  justify-center md:items-start lg:items-start items-center flex-col gap-10  text-btn">
+       <p class="font-roboto md:text-6xl lg:text-6xl text-4xl font-bold text-left">Work from anywhere</p>
+       <p class="font-roboto lg:text-left md:text-left text-center lg:text-xl md:text-sm text-base">In et dolor eu donec maecenas nulla. Cum sed orci, sit pellentesque quisque feugiat cras ullamcorper. Ultrices in amet, ullamcorper non viverra a, neque orci.</p>
+       <div class=" flex gap-5 items-center lg:justify-normal md:justify-normal justify-center">
+       <img src="../../assets/images/Apple.png" >
+       <img src="../../assets/images/google.png" >
+       </div>
+       
+     </div>
+     <div class=" flex justify-center">
+       <img src="../../assets/images/Phone1.png" alt="">
+     </div>
+   </div>
+   <div class=" bg-bl h-80 flex flex-col items-center justify-center text-white gap-10 ">
+     <p class="font-roboto md:text-6xl lg:text-6xl text-4xl font-bold text-center ">All the tech & design tools combined</P>
+     <p class="font-roboto  lg:text-4xl md:text-4xl text-base text-center">Turpis purus, imperdiet integer amet, eu.</P>
+     <button class=" bg-btn cursor-pointer px-7 py-2 rounded-lg text-white font-bold hover:text-btn hover:bg-white hover:border-btn hover:border-2 border-2 border-btn">Request Demo</button>
+   </div>
+   <div class=" flex items-center justify-center flex-col gap-14  text-btn py-20">
+       <p class="font-roboto md:text-6xl lg:text-6xl  text-4xl font-bold text-center">Get your business to grow fast</p>
+       <p class="font-roboto text-center leading-loose lg:text-xl md:text-xl text-base max-w-[900px]">Aliquet id quam amet, augue netus tristique elementum eros urna. Dignissim nisl mauris cras feugiat
+         congue at euismod donec.Lectus interdum nibh laoreet nunc bibendum volutpat.</p>
+       <div class=" mx-10 relative">
+         
+         <img src="../../assets/images/rec.png" class=" absolute m-auto left-0 right-0 top-0 bottom-0 ">
+         <img src="../../assets/images/Desktop.png" class=" px-10 z-10">
+       </div>
+         
+   </div>
+   <div class=" text-txt grid xl:grid-cols-3 2xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 px-10 gap-10">
+     <div class=" flex flex-col items-center justify-center">
+       <img src="../../assets/images/watch.png" alt="">
+       <p class="font-roboto text-2xl font-bold">Nisl arcu nunc</p>
+       <p class="font-roboto text-lg w-96  text-center">Dui consectetur gravida platea ut dis diam. Enim morbi proin auctor et.</p>
+     </div>
+     <div class=" flex flex-col items-center justify-center">
+       <img src="../../assets/images/tourn.png" alt="">
+       <p class="font-roboto text-2xl font-bold">Dui scelerisque</p>
+       <p class="font-roboto text-lg w-96 text-center">Et sit duis vestibulum proin. Sollicitudin velit, etiam a feugiat sagittis.</p>
+     </div>
+     <div class="font-roboto flex flex-col items-center justify-center">
+       <img src="../../assets/images/three-stars.png" alt="">
+       <p class="font-roboto text-2xl font-bold">Ac proin at</p>
+       <p class="font-roboto text-lg w-96 text-center">Elit purus magna donec mattis amet, leo varius sed. Ut metus sed convallis.</p>
+     </div>
+   </div>
+     
+   <div class=" bg-amber-400 mt-20 grid lg:grid-cols-2 w-full sm:grid-cols-1 md:grid-cols-1 gap-10 ">
+     <div class=" flex flex-col items-start  justify-center py-5 px-5 gap-1">
+       <p class="font-roboto lg:text-2xl md:text-2xl text-xl text-justify ">Id urna, nisl, ut quam. Diam suspendisse fringilla quam arcu mattis est velit in. Nibh in purus sit convallis phasellus ut. At vel erat ultricies commodo. Neque suspendisse a habitasse commodo.</p>
+       <p class="font-roboto w-lg  text-xl text-left">Marie Poirot,</p>
+       <p class="font-roboto w-lg  text-xl font-bold text-left">Bigapp</p>
+     </div>
+     
+     <div class=" flex justify-center items-center py-5 ">
+       <img src="../../assets/images/Col.png" class=" w-2xs">
+     </div>
+     
+   </div>
+   <p class="font-roboto lg:text-6xl md:text-6xl sm:text-5xl text-5xl  text-center font-bold py-20">The features you were looking for</p>
+
+   <div class=" grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 p-10 gap-10 text-txt">
+
+    <div class=" flex flex-col items-centerpy-5 px-5 gap-5">
+      <img src="../../assets/images/Tech1.png" alt="">
+      <div>
+      <p class="font-roboto text-2xl font-bold">Pellentesque turpis vestibulum</p>
+      <p>Ullamcorper enim, diam eros sed et vitae sit. Platea porttitor consequat, lorem in arcu et.</p></div>
+    </div>
+
+    <div class=" flex flex-col items-centerpy-5 px-5 gap-5">
+      <img src="../../assets/images/Tech2.png" alt="">
+      <div>
+      <p class="font-roboto text-2xl font-bold">Tincidunt quisque</p>
+      <p>Blandit ut tortor non, imperdiet. Morbi quam facilisis a nec tortor.</p></div>
+    </div>
+
+    <div class=" flex flex-col items-centerpy-5 px-5 gap-5">
+      <img src="../../assets/images/Tech3.png" alt="">
+      <div>
+      <p class="font-roboto text-2xl font-bold">Tincidunt magna diam</p>
+      <p>Massa mi pharetra viverra risus ornare sit. Viverra tortor, nulla et tellus, nunc.</p></div>
+    </div>
+
+    <div class=" flex flex-col items-centerpy-5 px-5 gap-5">
+      <img src="../../assets/images/Tech4.png" alt="">
+      <div>
+      <p class="font-roboto text-2xl font-bold">Purus habitasse</p>
+      <p>Massa arcu ut id risus et. Neque mauris sagittis cursus tincidunt egestas faucibus amet adipiscing scelerisque.</p></div>
+    </div>
+
+    <div class=" flex flex-col items-centerpy-5 px-5 gap-5">
+      <img src="../../assets/images/Tech5.png" alt="">
+      <div>
+      <p class="font-roboto text-2xl font-bold">Dictum elementum</p>
+      <p>Ullamcorper aliquam maecenas sapien sapien tempus tincidunt urna tempus. Dignissim ut felis a dui a massa risus.</p></div>
+    </div>
+
+    <div class=" flex flex-col items-centerpy-5 px-5 gap-5">
+      <img src="../../assets/images/Tech6.png" alt="">
+      <div>
+      <p class="font-roboto text-2xl font-bold">Semper vulputate ut</p>
+      <p>Tortor arcu eget nisi neque, in nunc metus. Posuere in egestas pharetra euismod commodo vitae consectetur.</p></div>
+    </div>
+   </div>
+   <p class="font-roboto lg:text-6xl md:text-6xl sm:text-5xl text-5xl  text-center font-bold py-10 px-5">Technology design articles</p>
+   <p class="font-roboto lg:text-2xl md:text-2xl sm:text-lg text-lg  text-center pb-10">Felis, semper ullamcorper sed pretium tristique vulputate semper massa ultrices. Integer orci gravida blandit amet.</p>
+
+
+   <div class=" grid xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-10 px-10 py-10">
+    <div class=" border-2 rounded-2xl ">
+        <div class=""> <img src="../../assets/images/Design.png" class=" w-full rounded-t-2xl"></div>
+        <div class=" w-fit px-10 py-5 flex flex-col gap-4">
+          <p class="font-roboto font-bold text-lg">Title</p>
+          <p>Egestas elit dui scelerisque ut eu purus aliquam vitae habitasse.</p>
+          <div class=" text-purple flex items-center  gap-4 cursor-pointer">
+            <button class=" cursor-pointer">Read article</button>
+            <img src="../../assets/images/arrow.png" class=" size-4 ">
+          </div>
+        </div>
+      </div>
+
+      <div class=" border-2 rounded-2xl ">
+        <div > <img src="../../assets/images/Design2.png" class=" w-full rounded-t-2xl"></div>
+        <div class=" w-fit px-10 py-5 flex flex-col gap-4">
+          <p class="font-roboto font-bold text-lg">Title</p>
+          <p>Egestas elit dui scelerisque ut eu purus aliquam vitae habitasse.</p>
+          <div class=" text-purple flex items-center  gap-4 cursor-pointer">
+            <button class=" cursor-pointer">Read article</button>
+            <img src="../../assets/images/arrow.png" class=" size-4 ">
+          </div>
+        </div>
+      </div>
+
+      <div class=" border-2 rounded-2xl ">
+        <div class=""> <img src="../../assets/images/Design3.png" class=" w-full rounded-t-2xl"></div>
+        <div class=" w-fit px-10 py-5 flex flex-col gap-4">
+          <p class="font-roboto font-bold text-lg">Title</p>
+          <p>Egestas elit dui scelerisque ut eu purus aliquam vitae habitasse.</p>
+          <div class=" text-purple flex items-center  gap-4 cursor-pointer">
+            <button class=" cursor-pointer">Read article</button>
+            <img src="../../assets/images/arrow.png" class=" size-4 ">
+          </div>
+        </div>
+      </div>
+   </div>
+   <p class="font-roboto lg:text-6xl md:text-6xl sm:text-5xl text-5xl  text-center font-bold py-10 px-5">Frequently asked questions</p>
+
+   <div class=" px-10 text-txt py-10">
+      <div class=" flex items-center  justify-between py-8 border-t-2 border-gray-300">
+        <p>Enim sodales consequat adipiscing facilisis massa venenatis, non lorem lobortis?</p>
+        <img src="../../assets/images/down.png" class=" size-6">
+      </div>
+      <div class=" flex items-center  justify-between py-8 border-t-2 border-gray-300">
+        <p>Venenatis nulla sagittis nunc, lobortis nec sollicitudin neque, dolor?</p>
+        <img src="../../assets/images/down.png" class=" size-6">
+      </div>
+      <div class=" flex items-center  justify-between py-8 border-t-2 border-gray-300">
+        <p>Varius ultricies molestie tellus fermentum, viverra ipsum scelerisque etiam lorem?</p>
+        <img src="../../assets/images/down.png" class=" size-6">
+      </div>
+      <div class=" flex items-center  justify-between py-8 border-t-2 border-gray-300">
+        <p>Nulla etiam vitae, at sagittis, nibh ultrices mattis feugiat faucibus?</p>
+        <img src="../../assets/images/down.png" class=" size-6">
+      </div>
+      <div class=" flex items-center  justify-between py-8 border-t-2 border-gray-300">
+        <p>Sagittis consectetur gravida nec turpis eros, id sit et, dictum?</p>
+        <img src="../../assets/images/down.png" class=" size-6">
+      </div>
+   </div>
+
+   <div class=" bg-bg h-80 flex flex-col items-center justify-center text-white gap-10 ">
+     <p class="font-roboto md:text-5xl lg:text-5xl text-4xl font-bold text-center ">Ready for your next project ?</P>
+     <p class="font-roboto  lg:text-3xl md:text-3xl text-xl text-center">Sit elit feugiat turpis sed integer integer accumsan turpis.</P>
+     <button class=" bg-btn cursor-pointer px-7 py-2 rounded-lg text-white font-bold hover:text-btn hover:bg-white hover:border-btn hover:border-2 border-2 border-btn">Request Demo</button>
+   </div>
+   <footer class="bg-white p-10 text-gray-700 mt-auto">
+    <div class="max-w-7xl mx-auto">
+      <!-- Top Footer Grid -->
+      <div class="grid grid-cols-1 md:grid-cols-5 gap-6">
+        <!-- Logo Section -->
+        <div class="flex flex-col">
+          <div class="flex items-center gap-2">
+  <img src="../../assets/images/Logo.png " alt="Protech Logo" class="w-7">
+  <span class="text-xl font-bold text-primary color: #701A75;">protech</span>
+</div>
+   </div>
+
+        <!-- Product Section -->
+        <div>
+          <h3 class="text-gray-500 font-semibold mb-3">PRODUCT</h3>
+          <ul class="space-y-4">
+            <li>Pricing</li>
+            <li>Overview</li>
+            <li>Browse</li>
+            <li>Accessibility</li>
+            <li>Five</li>
+          </ul>
+        </div>
+
+        <!-- Solutions Section -->
+        <div>
+          <h3 class="text-gray-500 font-semibold mb-3">SOLUTIONS</h3>
+          <ul class="space-y-4">
+            <li>Brainstorming</li>
+            <li>Ideation</li>
+            <li>Wireframing</li>
+            <li>Research</li>
+          </ul>
+        </div>
+
+        <!-- Resources Section -->
+        <div>
+          <h3 class="text-gray-500 font-semibold mb-3">RESOURCES</h3>
+          <ul class="space-y-4">
+            <li>Help Center</li>
+            <li>Blog</li>
+            <li>Tutorials</li>
+          </ul>
+        </div>
+
+        <!-- Company Section -->
+        <div>
+          <h3 class="text-gray-500 font-semibold mb-3">COMPANY</h3>
+          <ul class="space-y-4">
+            <li>About</li>
+            <li>Press</li>
+            <li>Events</li>
+            <li>Careers</li>
+          </ul>
+        </div>
+      </div>
+
+      <!-- Bottom Footer -->
+      <div class="border-t mt-6 pt-6 text-sm flex flex-col md:flex-row justify-between items-center">
+        <!-- Copyright Text -->
+        <p class="font-robototext-gray-500">@ 2023 Protech, Inc. All rights reserved.</p>
+
+        <!-- Links -->
+        <div class="flex gap-6">
+          <a href="#" class="hover:underline text-gray-500">Terms</a>
+          <a href="#" class="hover:underline text-gray-500">Privacy</a>
+          <a href="#" class="hover:underline text-gray-500">Contact</a>
+        </div>
+
+        <!-- Social Media Icons -->
+        <div class="flex gap-5 mt-4 md:mt-0">
+          <img src="../../assets/images/youtube.png" alt="YouTube" class="w-5 ">
+          <img src="../../assets/images/facebook.png" alt="Facebook" class="w-5">
+          <img src="../../assets/images/twitter.png" alt="Twitter" class="w-5">
+          <img src="../../assets/images/instagram.png" alt="Instagram" class="w-5">
+          <img src="../../assets/images/facebook.png" alt="LinkedIn" class="w-5">
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+</script>
